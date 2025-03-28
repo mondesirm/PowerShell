b828699cc7d6fb665e1ca21d0e98e72ddbe36c92
MONDESIR Malik <malikmondesir@gmail.com>
2025-03-28 23:17:00 +0100

    add port related functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,14 @@ Import-Module Microsoft.WinGet.CommandNotFound
 Set-Alias pn pnpm
 Set-PoshTheme bubbles
 
+function art { php artisan $args }
+function tinker { art tinker $args }
+function pest { ./vendor/bin/pest $args }
+function pint { ./vendor/bin/pint $args }
+
+function port { netstat -ano | sls 'tcp' | sls 'listening' | sls $args[0] }
+function pkill { port $args | % { Stop-Process -Id $_.ToString().Split()[-1] } }
+
 $env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path', 'User')
 
 . (Join-Path -Path (Split-Path -Path $PROFILE -Parent) -ChildPath 'gh-copilot.ps1')
